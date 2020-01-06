42a419d67974802db13e6449a1329ac7d81f18dd
Ryan Hoffman <ryho8890@colorado.edu>
2020-01-06 15:05:35 -0700

    Update Files from PC

---
+++ A7main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "SpellChecker.h"
+#include "WordCounts.h"
+
+using namespace std;
+
+int main()
+{
+/*
+    SpellChecker spellCheck("English", "VALID_WORDS_3000.txt", "MISSPELLED.txt");
+    SpellChecker spellCheck1("English");
+    SpellChecker spellCheck2;
+
+    spellCheck1.readValidWords("VALID_WORDS_3000.txt");
+    spellCheck1.readCorrectedWords("MISSPELLED.txt");
+
+    spellCheck2.readValidWords("VALID_WORDS_3000.txt");
+    spellCheck2.readCorrectedWords("MISSPELLED.txt");
+
+    spellCheck.setStartMarker('~');
+    spellCheck.setEndMarker('~');
+
+    spellCheck1.setStartMarker('$');
+    spellCheck1.setEndMarker('$');
+
+    spellCheck2.setStartMarker('*');
+    spellCheck2.setEndMarker('*');
+
+    cout << spellCheck.repair("todayy") << endl;
+    cout << spellCheck.repair("todayy, is teh day!") << endl;
+    cout << spellCheck.repair("ahsjdklfha") << endl;
+    cout << spellCheck.repair("tomor is another day!") << endl;
+    cout << spellCheck.repair("Teh brown asdhf jumped.") << endl;
+    cout << spellCheck.repair("Hel'lo, my na!me is dav!i.d") << endl;
+
+    cout << spellCheck1.repair("todayy") << endl;
+    cout << spellCheck1.repair("todayy, is teh day!") << endl;
+    cout << spellCheck1.repair("ahsjdklfha") << endl;
+    cout << spellCheck1.repair("tomor is another day!") << endl;
+    cout << spellCheck1.repair("Teh brown asdhf jumped.") << endl;
+    cout << spellCheck1.repair("Hel'lo, my na!me is dav!i.d") << endl;
+
+    cout << spellCheck2.repair("todayy") << endl;
+    cout << spellCheck2.repair("todayy, is teh day!") << endl;
+    cout << spellCheck2.repair("ahsjdklfha") << endl;
+    cout << spellCheck2.repair("tomor is another day!") << endl;
+    cout << spellCheck2.repair("Teh brown asdhf jumped.") << endl;
+    cout << spellCheck2.repair("Hel'lo, my na!me is dav!i.d") << endl;
+*/
+    return 0;
+}
